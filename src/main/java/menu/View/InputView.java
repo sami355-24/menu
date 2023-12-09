1fef66a7611854caa756d9bc513d5190adf86de8
sami355-24 <ddung355@gmail.com>
2023-12-09 13:29:37 +0900

    feat: 프로그램 시작시 출력할 문구 출력하는 기능 추가

--- src/main/java/menu/View/InputView.java
+++ src/main/java/menu/View/InputView.java
@@ -1,5 +1,10 @@
 package menu.View;
 
+import static menu.Message.Prompt.WELL_COME_MESSAGE;
+
 public class InputView {
 
+    public void printWellComeMessage() {
+        System.out.println(WELL_COME_MESSAGE);
+    }
 }
