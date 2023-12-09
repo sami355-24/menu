14e70f9bdef1338c592fc7bc34ad0ac069c1a85a
sami355-24 <ddung355@gmail.com>
2023-12-09 12:23:39 +0900

    feat: 예외를 나타내는 클래스를 관리하는 패키지 생성

---
+++ src/main/java/menu/Exception/CommonValidateException.java
@@ -0,0 +1,8 @@
+package menu.Exception;
+
+public class CommonValidateException extends IllegalArgumentException{
+
+    public CommonValidateException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
