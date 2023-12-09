f6e1c57435d405fabaeb037e10147403d9bfb5de
sami355-24 <ddung355@gmail.com>
2023-12-09 13:08:46 +0900

    feat: 메뉴 추천시 사용할 문구를 관리하는 패키지 생성

---
+++ src/main/java/menu/Message/Prompt.java
@@ -0,0 +1,11 @@
+package menu.Message;
+
+
+public class Prompt {
+
+    public static final String WELL_COME_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    public static final String INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    public static final String INPUT_BANNED_FOOD = "(이)가 못 먹는 메뉴를 입력해 주세요.";
+    public static final String RECOMMEND_RESULT = "메뉴 추천 결과입니다.";
+    public static final String END_MESSAGE = "추천을 완료했습니다.";
+}
